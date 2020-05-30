bd1306346befe35a4fb9500af879a061ec54f904
amoralozano <66109164+amoralozano@users.noreply.github.com>
2020-05-29 20:57:12 -0700

    initial xcode project creation

---
+++ youtube-next/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-next
+//
+//  Created by Abdael Mora on 5/29/20.
+//  Copyright © 2020 Abdael Mora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
